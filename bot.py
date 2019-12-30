714dd47e8942ce0ffbebdb412d00f893b5168210
Andruida <borus.benedek@gmail.com>
2019-12-30 22:28:56 +0100

    Fixed log flood bug

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import sqlalchemy as sql
 from threading import Thread, Event, Timer
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+#logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
